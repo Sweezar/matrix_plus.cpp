9465cbd80715a225657f7c7ca7b7fcc99397c2dd
Sweezar <royal13flush@gmail.com>
2024-04-15 18:15:04 +0300

    Добавление реализации некоторых методов класса

--- src/s21_matrix_oop.cpp
+++ src/s21_matrix_oop.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-void _S21Matrix_memory_allocate(int rows, int cols, double** matrix) {
+void S21Matrix::_S21Matrix_memory_allocate(int rows, int cols, double**& matrix) {
   if(rows <= 0 || cols <= 0) {
-    throw invalid_argument("Количество столбцов и строк матрицы должны быть больше нуля");
+    throw invalid_argument("The count of matrix columns and rows must be greater than zero");
   }
   matrix = new double*[rows]();
   if(!matrix) {
@@ -23,11 +23,27 @@ void _S21Matrix_memory_allocate(int rows, int cols, double** matrix) {
   }
 }
 
+void S21Matrix::_destroy() {
+  for(int i = 0; i < _rows; i++) {
+    delete[] _matrix[i];
+  }
+  delete[] _matrix;
+}
+
 S21Matrix::S21Matrix() : _rows(3), _cols(3) {
   try {
     _S21Matrix_memory_allocate(_rows, _cols, _matrix);
   } catch (const bad_alloc& e) {
-    cerr << "Ошибка выделения памяти";
+    cerr << "Memory allocation error";
+    exit(1);
+  }
+}
+
+S21Matrix::S21Matrix(int rows) : _rows(rows), _cols(rows) {
+  try {
+    _S21Matrix_memory_allocate(_rows, _cols, _matrix);
+  } catch (const bad_alloc& e) {
+    cerr << "Memory allocation error";
     exit(1);
   }
 }
@@ -36,55 +52,169 @@ S21Matrix::S21Matrix(int rows, int cols) : _rows(rows), _cols(cols) {
   try {
     _S21Matrix_memory_allocate(_rows, _cols, _matrix);
   } catch (const bad_alloc& e) {
-    cerr << "Ошибка выделения памяти";
+    cerr << "Memory allocation error";
     exit(1);
   }
 }
 
-S21Matrix::S21Matrix(const S21Matrix& o) : _rows(o._rows), _cols(o._cols) {
+// copy
+S21Matrix::S21Matrix(const S21Matrix& other) : _rows(other._rows), _cols(other._cols) {
   try {
     _S21Matrix_memory_allocate(_rows, _cols, _matrix);
   } catch (const bad_alloc& e) {
-    cerr << "Ошибка выделения памяти";
+    cerr << "Memory allocation error";
     exit(1);
   }
 
   for(int i = 0; i < _rows; i++) {
-    memcpy(_matrix[i], o._matrix[i], _cols * sizeof(double));
+    memcpy(_matrix[i], other._matrix[i], _cols * sizeof(double));
   }
 }
 
-S21Matrix::S21Matrix(S21Matrix&& o) : _rows(o._rows), _cols(o._cols), _matrix(o._matrix) {
-  o._rows = 0;
-  o._cols = 0;
-  o._matrix = nullptr;
+// move
+S21Matrix::S21Matrix(S21Matrix&& other) {
+  _rows = other._rows;
+  _cols = other._cols;
+  _matrix = other._matrix;
+
+  other._rows = 0;
+  other._cols = 0;
+  other._matrix = nullptr;
 }
 
 S21Matrix::~S21Matrix() {
-  for(int i = 0; i < _rows; i++) {
-    delete[] _matrix[i];
-  }
-  delete[] _matrix;
+  _destroy();
 }
 
-void S21Matrix::sum_matrix(const S21Matrix& o) {
-  if(_matrix == nullptr || o._matrix == nullptr) {
-    throw invalid_argument("Матрица отсутствует");
+S21Matrix& S21Matrix::operator=(const S21Matrix& other) {
+  if(!(EqMatrix(other))) {
+    _destroy();
+
+    _rows = other._rows;
+    _cols = other._cols;
+    try {
+      _S21Matrix_memory_allocate(_rows, _cols, _matrix);
+    } catch (const bad_alloc& e) {
+      cerr << "Memory allocation error";
+      exit(1);
+    }
+    for (int i = 0; i < _rows; ++i) {
+      for (int j = 0; j < _cols; ++j) {
+          _matrix[i][j] = other._matrix[i][j];
+      }
+    }
   }
-  if(_rows <= 0 || _cols <= 0 || o._rows <= 0 || o._cols <= 0) {
-    throw invalid_argument("Количество столбцов и строк матрицы должны быть больше нуля");
+
+  return *this;
+}
+
+
+bool S21Matrix::EqMatrix(const S21Matrix& other) {
+  if(_matrix == nullptr || other._matrix == nullptr) {
+    throw invalid_argument("Matrix missing");
   }
-  if(_rows != o._rows || _cols != o._cols) {
-    throw invalid_argument("размерность матриц не равна");
+  if(_rows <= 0 || _cols <= 0 || other._rows <= 0 || other._cols <= 0) {
+    throw invalid_argument("The count of matrix columns and rows must be greater than zero");
+  }
+  int isEqual = 1;
+  if(_rows != other._rows || _cols != other._cols) {
+    isEqual = 0;
+  }
+  for (int i = 0; i < _rows; i++) {
+    for (int ii = 0; ii < _cols; ii++) {
+      
+      if(_matrix[i][ii] != other._matrix[i][ii]) {
+        isEqual = 0;
+      }
+    }
+  }
+
+  return isEqual ? true : false;
+}
+
+void S21Matrix::SumMatrix(const S21Matrix& other) {
+  if(_matrix == nullptr || other._matrix == nullptr) {
+    throw invalid_argument("Matrix missing");
+  }
+  if(_rows <= 0 || _cols <= 0 || other._rows <= 0 || other._cols <= 0) {
+    throw invalid_argument("The count of matrix columns and rows must be greater than zero");
+  }
+  if(_rows != other._rows || _cols != other._cols) {
+    throw invalid_argument("The dimensions of the matrices are not equal");
   }
 
   for (int i = 0; i < _rows; i++) {
     for (int ii = 0; ii < _cols; ii++) {
-      _matrix[i][ii] = _matrix[i][ii] + o._matrix[i][ii];
+      _matrix[i][ii] = _matrix[i][ii] + other._matrix[i][ii];
     }
   }
 }
 
+void S21Matrix::SubMatrix(const S21Matrix& other) {
+  if(_matrix == nullptr || other._matrix == nullptr) {
+    throw invalid_argument("Matrix missing");
+  }
+  if(_rows <= 0 || _cols <= 0 || other._rows <= 0 || other._cols <= 0) {
+    throw invalid_argument("The count of matrix columns and rows must be greater than zero");
+  }
+  if(_rows != other._rows || _cols != other._cols) {
+    throw invalid_argument("The dimensions of the matrices are not equal");
+  }
+
+  for (int i = 0; i < _rows; i++) {
+    for (int ii = 0; ii < _cols; ii++) {
+      _matrix[i][ii] = _matrix[i][ii] - other._matrix[i][ii];
+    }
+  }
+}
+
+void S21Matrix::MulNumber(const double num) {
+  if(_matrix == nullptr) {
+    throw invalid_argument("Matrix missing");
+  }
+  if(_rows <= 0 || _cols <= 0) {
+    throw invalid_argument("The count of matrix columns and rows must be greater than zero");
+  }
+  for (int i = 0; i < _rows; i++) {
+    for (int ii = 0; ii < _cols; ii++) {
+      _matrix[i][ii] = _matrix[i][ii] * num;
+    }
+  }
+}
+
+void S21Matrix::MulMatrix(const S21Matrix& other) {
+  if(_matrix == nullptr) {
+    throw invalid_argument("Matrix missing");
+  }
+  if(_rows <= 0 || _cols <= 0) {
+    throw invalid_argument("The count of matrix columns and rows must be greater than zero");
+  }
+  if (_cols != other._rows) {
+    throw invalid_argument("the count of columns of the first matrix is ​​not equal to the count of rows of the second matrix");
+  }
+  S21Matrix tmp(_rows, other._cols);
+  for (int i = 0; i < _rows; i++) {
+    for (int ii = 0; ii < other._cols; ii++) {
+      tmp._matrix[i][ii] = 0;
+      for (int iii = 0; iii < other._rows; iii++) {
+        tmp._matrix[i][ii] += _matrix[i][iii] * other._matrix[iii][ii];
+      }
+    }
+  }
+
+  *this = move(tmp);
+}
+
+void S21Matrix::fill(double* a, int size) {
+  int index = 0;
+  for(int i = 0; i < _rows; i++) {
+    for(int ii = 0; ii < _cols; ii++) {
+      if(index < size) {
+        _matrix[i][ii] = a[index++];
+      }
+    }
+  }
+}
 
 void S21Matrix::print_matrix() {
   for (int i = 0; i < _rows; i++) {
@@ -93,5 +223,4 @@ void S21Matrix::print_matrix() {
     }
     cout << endl;
   }
-  
 }
--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -8,32 +8,41 @@ class S21Matrix
 private:
   int _rows, _cols;
   double **_matrix;
-  void _S21Matrix_memory_allocate(int rows, int cols, double** matrix);
+  void _S21Matrix_memory_allocate(int rows, int cols, double**& matrix);
+  void _destroy();
 
 public:
   S21Matrix();
+  S21Matrix(int rows);
   S21Matrix(int rows, int cols);
-  S21Matrix(const S21Matrix& o); // copy
-  S21Matrix(S21Matrix&& o); // move
+  S21Matrix(const S21Matrix& other); // copy
+  S21Matrix(S21Matrix&& other); // move
   ~S21Matrix();
 
-  S21Matrix& operator+(const S21Matrix& o);
-  S21Matrix& operator+=(const S21Matrix& o);
-  S21Matrix& operator-(const S21Matrix& o);
-  S21Matrix& operator-=(const S21Matrix& o);
-  S21Matrix& operator*(const S21Matrix& o);
-  S21Matrix& operator*=(const S21Matrix& o);
-  S21Matrix& operator=(const S21Matrix& o);
-  S21Matrix& operator==(const S21Matrix& o);
-  int& operator()(int row, int col);
-  int& operator()(int row, int col) const;
+  S21Matrix operator+(const S21Matrix& other);
+  S21Matrix& operator+=(const S21Matrix& other);
+  S21Matrix operator-(const S21Matrix& other);
+  S21Matrix& operator-=(const S21Matrix& other);
+  S21Matrix operator*(const S21Matrix& other);
+  S21Matrix& operator*=(const S21Matrix& other);
+  S21Matrix& operator=(const S21Matrix& other);
+  bool operator==(const S21Matrix& other);
+  double& operator()(int row, int col);
+  double& operator()(int row, int col) const;
 
-  bool eq_matrix(const S21Matrix& o);
-  void sum_matrix(const S21Matrix& o); 
-  void sub_matrix(const S21Matrix& o);
+  bool EqMatrix(const S21Matrix& other);
+  void SumMatrix(const S21Matrix& other); 
+  void SubMatrix(const S21Matrix& other);
+  void MulNumber(const double num);
+  void MulMatrix(const S21Matrix& other);
+  S21Matrix Transpose();
+  S21Matrix CalcComplements();
+  double Determinant();
+  S21Matrix InverseMatrix();
 
-
+  void fill(double* a, int size);
   void print_matrix();
+
 };
 
 #endif
